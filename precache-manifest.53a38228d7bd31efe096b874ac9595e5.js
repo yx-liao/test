df54d6b525cb8bbbcd9e8283e17fd565edb26354
yxliao-github-action <yxliao@github-actoin.com>
2020-03-20 04:43:27 +0000

    deploy to github pages

---
+++ precache-manifest.53a38228d7bd31efe096b874ac9595e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0ddc730e62cf985745d7a51f07e9bd",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "11d5f3bbd33e118fd478",
+    "url": "/test/static/css/main.f90cb68d.chunk.css"
+  },
+  {
+    "revision": "f51804f573e5396dd969",
+    "url": "/test/static/js/2.01173763.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/test/static/js/2.01173763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d5f3bbd33e118fd478",
+    "url": "/test/static/js/main.8490ec9f.chunk.js"
+  },
+  {
+    "revision": "e1584a28716753c4cdf1",
+    "url": "/test/static/js/runtime-main.da5d74ac.js"
+  }
+]);
